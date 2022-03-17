b215da70bbebbfaf1463acb96d459ba13a5d5af8
Vitor Mateus <vitor7jan@gmail.com>
2022-03-17 17:00:26 -0300

    correcao url backend

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-// const url = `https://originarios.herokuapp.com`;
-export const url = `http://localhost:8080`;
+export const url = `https://originarios.herokuapp.com`;
 
 const api = axios.create({
   baseURL: `${url}/api`,
